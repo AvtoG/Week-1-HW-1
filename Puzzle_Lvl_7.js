5e8cdadf1b6df9592d212686850357b3a9ba9082
AvtoG <agrigolia@cca.edu>
2017-01-22 15:46:33 -0800

    Create Puzzle_Lvl_7.js

---
+++ Puzzle_Lvl_7.js
@@ -0,0 +1,34 @@
+while (getColor() != "red") {
+  down();
+} 
+if (getColor() == "red") {
+  right();
+  right();
+}
+while (getColor() != "red") {
+  up();
+}
+if (getColor() == "red") {
+  right();
+  right();
+}
+while (getColor() != "red") {
+  down();
+} 
+if (getColor() == "red") {
+  right();
+  right();
+}
+while (getColor() != "red") {
+  up();
+}
+if (getColor() == "red") {
+  right();
+  right();
+}
+while (getColor() != "red") {
+  down();
+} 
+if (getColor() == "red") {
+  right();
+  right();
